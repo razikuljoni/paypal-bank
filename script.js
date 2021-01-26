200893cbed49dba6c09598cad5ea112c840c5b7c
Md. Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2021-01-26 15:41:09 +0600

    complete functionality

--- script.js
+++ script.js
@@ -7,9 +7,34 @@ loginBtn.addEventListener("click", function () {
     transitionArea.style.display = 'block';
 })
 
-////diposite area
-var dipositeArea = document.getElementById('diposite');
-dipositeArea.addEventListener('click', function () {
-    const dipositeAmount = document.getElementById('dipositeAmount').value;
-    const dipositeNumber = parseFloat(dipositeAmount);
+//deposit area
+var depositArea = document.getElementById('deposit');
+depositArea.addEventListener('click', function () {
+
+    const depositNumber = getInputNumber('depositAmount');
+
+    updateSpanText('cDeposit', depositNumber);
+    updateSpanText('cBalance', depositNumber);
+    document.getElementById("depositAmount").value = '';
 })
+//withdraw functionality
+const withdrawButton = document.getElementById('wBtn');
+withdrawButton.addEventListener('click', function () {
+    const wAmountNumber = getInputNumber("wAmount");
+    updateSpanText('cWithdraw', wAmountNumber);
+    updateSpanText('cBalance', -1 * wAmountNumber);
+    document.getElementById("wAmount").value = "";
+})
+
+function getInputNumber(id) {
+    const withdrawAmount = document.getElementById(id).value;
+    const wAmountNumber = parseFloat(withdrawAmount);
+    return wAmountNumber;
+}
+
+function updateSpanText(id, depositNumber) {
+    const currentBalance = document.getElementById(id).innerText;
+    const cBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = depositNumber + cBalanceNumber;
+    document.getElementById(id).innerText = totalBalance;
+}
